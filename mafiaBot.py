07465d29390c0f3ff87eb37cdb27c784fe342ae5
mrprogremmer010 <72856158+mrprogremmer010@users.noreply.github.com>
2020-12-23 16:38:30 +0200

    Update mafiaBot.py

--- mafiaBot.py
+++ mafiaBot.py
@@ -39,30 +39,59 @@ async def join(ctx):
         choices = " ".join(players.keys())
         await mur()
         await dv()
-        await channel.send(f"{author} you are on the game please wait for more {10-len(players)} players")
+        await channel.send(f"{author} you enterd the game please wait for more {10-len(players)} players")
         await channel.send(f"now there is {choices}")
         print(players)
 
-    if len(players) == 2:
+    if len(players) == 1:
         for id in players_id:
             member = await client.fetch_user(int(id))
             await member.send(f"{pc_choice}")
-            await detective()
+
+        if players[author] == "detective":
+            await member.send(f"{players}")
+
         if players[author] == "murder":
             @client.command()
             async def kill(ctx):
                 await ctx.message.delete()
                 killed = random.choice(list(players.values()))
+                killed_id = await client.fetch_user(int(id))
                 await ctx.send(f"{killed.mention} has been killed")
-                killed.pop(players)
-                killed.remove(players_id)
+                players.pop(killed)
+                players_id.remove(killed_id)
                 await ctx.send(f"{players}")
-        else:
-            await ctx.send("you are not egiable to do this")
+                if players[author] != "murder":
+                    await member.send("you are not egiable to do this")
 
-async def detective():
-    if players[author] == "detective":
-        ctx.send(f"{players}")
+    @client.command()
+    async def leave(ctx):
+        await ctx.send("are you sure you want to leave? y/n")
+        answer = await client.wait_for("message")
+        if answer.content == "y":
+            await ctx.send(f"{author} has been quit")
+            players.pop(author)
+            players_id.remove(author_id)
+            play = " ".join(players.values())
+            await channel.send(f"now there is {play}")
+            return answer
+        elif answer.content == "n":
+            await ctx.send("ok we are glad you here")
+            return answer
+        if answer.author.mention != author:
+            answer= await client.wait_for('message')
+        elif answer.author.mention == author:
+            massageStatus = False
+        else:
+            await ctx.send("invaild choice")
+            answer = await client.wait_for("message")
+        return answer
+
+
+
+
+
+
 
 async def mur():
     if "murder" in players:
